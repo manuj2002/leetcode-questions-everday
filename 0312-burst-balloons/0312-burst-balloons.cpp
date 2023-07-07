26227ea31e0b4aab0296d2aa98e120ec15b5ae58
manuj2002 <98848923+manuj2002@users.noreply.github.com>
2023-07-08 01:54:24 +0530

    Time: 429 ms (75.24%), Space: 10.1 MB (84.85%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
 
-int dfs(int i,int j,vector<int>& nums,vector<vector<int>>&dp)
-{
-  if(dp[i][j]!=-1)
-     return(dp[i][j]);
-    if(j<i)
-      return(dp[i][j]=0);
-      int ans=0;
-      for(int k=i;k<=j;k++)
-      {
-         int x=nums[k];
-         if(i-1>=0)
-          x*=nums[i-1];
-          if(j+1<nums.size())
-          x*=nums[j+1];
-          if(i<=k-1)
-          {
-            x+=dfs(i,k-1,nums,dp);
-          }
-          if(k+1<=j)
-          {
-            x+=dfs(k+1,j,nums,dp);
-          }
-          ans=max(ans,x);
-      }
-      return(dp[i][j]=ans);
-}
 
 
     int maxCoins(vector<int>& nums) {
   int n=nums.size();
-  vector<vector<int>>dp(n+2,vector<int>(n+2,-1));
-       return(dfs(0,nums.size()-1,nums,dp));
+  vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i;j<n;j++)
+            {
+                int ans=0;
+                for(int k=i;k<=j;k++)
+                {
+                   int x=nums[k] ;
+                    if(i-1>=0)
+                        x*=nums[i-1];
+                    if(j+1<n)
+                        x*=nums[j+1];
+                    if(i<=k-1)
+                        x+=dp[i][k-1];
+                    if(k+1<=j)
+                        x+=dp[k+1][j];
+                    ans=max(ans,x);
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return(dp[0][n-1]);
+      
     }
 };
